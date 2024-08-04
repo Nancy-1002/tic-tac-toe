031724d71b64c0eab2bd674984a5f7fe30667eaf
Nancy-1002 <104241261+Nancy-1002@users.noreply.github.com>
2024-08-04 10:17:18 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,101 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const Square = (props) => {
+  return (
+    <button
+      className='square'
+      onClick={props.onClickEvent}
+    >
+      {props.value}
+    </button>
+  );
+};
+
+const Board = () => {
+  const initialSquares = Array(9).fill(null);
+  const [squares, setSquares] = useState(initialSquares);
+  const [xisNext,setXisNext] = useState(true);
+
+  const handleClick = (i) => {
+    const newSquares = [...squares];
+
+    const winnerDeclared=Boolean(calculateWinner(newSquares));
+    const squareFilled = Boolean(newSquares[i]);
+
+    if(winnerDeclared || squareFilled) {
+      return;
+    }
+
+    newSquares[i] = xisNext ? 'X' : 'O';
+    setSquares(newSquares);
+    setXisNext(!xisNext);
+  };
+
+  const renderSquare = (i) => {
+    return (
+      
+      <Square 
+        value={squares[i]}
+        onClickEvent={() => handleClick(i)}
+      />
+    );
+  };
+
+  const winner=calculateWinner(squares);
+  const status=winner ?
+  `Winner : ${winner}` :
+  `Next Player : ${xisNext ? 'X' : 'O'}`;
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className='board-row'>
+        {renderSquare(0)}{renderSquare(1)}{renderSquare(2)}
+      </div>
+      <div className='board-row'>
+        {renderSquare(3)}{renderSquare(4)}{renderSquare(5)}
+      </div>
+      <div className='board-row'>
+        {renderSquare(6)}{renderSquare(7)}{renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+const Game = () => {
+  return (
+    <div className="game">
+      TIC-TAC-TOE
+      <Board />
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let line of lines){
+    const [a, b, c] = line;
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+
+  return null;
+}
